e3423461461e0f9d3284f03dead05d1ff93b9ed9
SicdeX <sicdex@gmail.com>
2022-11-25 19:16:35 +0600

    see desc
    - add eslint
    - style code

--- Entities.ts
+++ Entities.ts
@@ -1,48 +1,63 @@
-import { Ability, ArrayExtensions, Color, Creep, DOTA_RUNES, Entity, EntityManager, EventsSDK, GUIInfo, Hero, LocalPlayer, ParticlesSDK, RendererSDK, Rune, Tree, Unit, Vector2 } from "github.com/octarine-public/wrapper/index"
+import {
+	Ability,
+	ArrayExtensions,
+	Color,
+	Creep,
+	DOTA_RUNES,
+	Entity,
+	EntityManager,
+	EventsSDK,
+	GUIInfo,
+	Hero,
+	LocalPlayer,
+	ParticlesSDK,
+	RendererSDK,
+	Rune,
+	Tree,
+	Unit,
+	Vector2
+} from "github.com/octarine-public/wrapper/index"
+
 import { RootMenu } from "./menu"
 
 function UnitPath(name: string): string {
-	if (name.includes("npc_dota_hero_"))
-		return `panorama/images/heroes/${name}_png.vtex_c`
+	if (name.includes("npc_dota_hero_")) return `panorama/images/heroes/${name}_png.vtex_c`
 
 	if (name.includes("_courier"))
-		return `github.com/octarine-private/immortal-core/scripts_files/images/couriers/radiant.png`
+		return "github.com/octarine-private/immortal-core/scripts_files/images/couriers/radiant.png"
 
 	if (name.includes("badguys_tower") || name.includes("goodguys_tower"))
 		return "panorama/images/heroes/npc_dota_hero_tower_radiant_png.vtex_c"
 
-	if (name.includes("eidolon"))
-		return `panorama/images/heroes/npc_dota_eidolon_png.vtex_c`
+	if (name.includes("eidolon")) return "panorama/images/heroes/npc_dota_eidolon_png.vtex_c"
 
 	if (name.includes("familiar"))
-		return `panorama/images/heroes/npc_dota_visage_familiar_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_visage_familiar_png.vtex_c"
 
 	if (name.includes("shaman_ward"))
-		return `panorama/images/heroes/npc_dota_shadow_shaman_ward_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_shadow_shaman_ward_png.vtex_c"
 
-	if (name.includes("golem"))
-		return `panorama/images/heroes/npc_dota_warlock_golem_png.vtex_c`
+	if (name.includes("golem")) return "panorama/images/heroes/npc_dota_warlock_golem_png.vtex_c"
 
 	if (name.includes("creep_badguys"))
-		return `panorama/images/heroes/npc_dota_hero_creep_dire_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_hero_creep_dire_png.vtex_c"
 
 	if (name.includes("creep_goodguys"))
-		return `panorama/images/heroes/npc_dota_hero_creep_radiant_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_hero_creep_radiant_png.vtex_c"
 
 	if (name.includes("druid_bear"))
-		return `panorama/images/heroes/npc_dota_lone_druid_bear_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_lone_druid_bear_png.vtex_c"
 
-	if (name.includes("lycan_wolf"))
-		return `panorama/images/heroes/npc_dota_lycan_wolf_png.vtex_c`
+	if (name.includes("lycan_wolf")) return "panorama/images/heroes/npc_dota_lycan_wolf_png.vtex_c"
 
 	if (name.includes("undying_zombie"))
-		return `panorama/images/heroes/npc_dota_unit_undying_zombie_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_unit_undying_zombie_png.vtex_c"
 
 	if (name.includes("necronomicon_archer"))
-		return `panorama/images/heroes/npc_dota_necronomicon_archer_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_necronomicon_archer_png.vtex_c"
 
 	if (name.includes("necronomicon_warrior"))
-		return `panorama/images/heroes/npc_dota_necronomicon_warrior_png.vtex_c`
+		return "panorama/images/heroes/npc_dota_necronomicon_warrior_png.vtex_c"
 
 	if (name.includes("roshan"))
 		return "panorama/images/spellicons/roshan_halloween_angry_png.vtex_c"
@@ -51,26 +66,19 @@ function UnitPath(name: string): string {
 }
 
 function GetEntityTeamColor(ent: Entity): Color {
-	if (ent.IsNeutral)
-		return Color.White
-	if (ent.IsEnemy())
-		return Color.Red
+	if (ent.IsNeutral) return Color.White
+	if (ent.IsEnemy()) return Color.Red
 	return Color.Green
 }
 
-function RenderEntity(
-	ent: Entity,
-	size: Vector2,
-	path: string,
-): void {
+function RenderEntity(ent: Entity, size: Vector2, path: string): void {
 	const screen_pos = RendererSDK.WorldToScreen(ent.Position)
-	if (screen_pos === undefined)
-		return
+	if (screen_pos === undefined) return
 	screen_pos.SubtractForThis(size.DivideScalar(2))
 	RendererSDK.FilledCircle(
 		screen_pos.SubtractScalar(2),
 		size.AddScalar(4),
-		GetEntityTeamColor(ent),
+		GetEntityTeamColor(ent)
 	)
 	RendererSDK.Image(path, screen_pos, 0, size)
 }
@@ -79,25 +87,18 @@ const Trees = EntityManager.GetEntitiesByClass(Tree)
 function DrawTrees(size: Vector2): void {
 	const tree_size = size.DivideScalar(2)
 	for (const tree of Trees) {
-		if (!tree.IsAlive)
-			continue
+		if (!tree.IsAlive) continue
 		const screen_pos = RendererSDK.WorldToScreen(tree.Position)
-		if (screen_pos === undefined)
-			continue
+		if (screen_pos === undefined) continue
 		screen_pos.SubtractForThis(tree_size.DivideScalar(2))
-		RendererSDK.FilledCircle(
-			screen_pos,
-			tree_size,
-			Color.Yellow,
-		)
+		RendererSDK.FilledCircle(screen_pos, tree_size, Color.Yellow)
 	}
 }
 
 const Creeps = EntityManager.GetEntitiesByClass(Creep)
 function DrawCreeps(size: Vector2): void {
 	for (const creep of Creeps) {
-		if (/*!creep.IsVisible || */!creep.IsAlive/* || !creep.IsSpawned*/)
-			continue
+		if (/*!creep.IsVisible || */ !creep.IsAlive /* || !creep.IsSpawned*/) continue
 		RenderEntity(creep, size, UnitPath(creep.Name))
 	}
 }
@@ -105,15 +106,13 @@ function DrawCreeps(size: Vector2): void {
 const Heroes = EntityManager.GetEntitiesByClass(Hero)
 function DrawHeroes(size: Vector2): void {
 	for (const hero of Heroes)
-		if (hero.IsVisible && hero.IsAlive)
-			RenderEntity(hero, size, UnitPath(hero.Name))
+		if (hero.IsVisible && hero.IsAlive) RenderEntity(hero, size, UnitPath(hero.Name))
 }
 
 const Runes = EntityManager.GetEntitiesByClass(Rune)
 function DrawRunes(size: Vector2): void {
 	for (const rune of Runes) {
-		if (!rune.IsVisible)
-			continue
+		if (!rune.IsVisible) continue
 		let rune_name: string
 		switch (rune.Type) {
 			case DOTA_RUNES.DOTA_RUNE_DOUBLEDAMAGE:
@@ -140,36 +139,33 @@ function DrawRunes(size: Vector2): void {
 			default:
 				continue
 		}
-		RenderEntity(rune, size, `github.com/octarine-private/immortal-core/scripts_files/images/runes/${rune_name}.png`)
+		RenderEntity(
+			rune,
+			size,
+			`github.com/octarine-private/immortal-core/scripts_files/images/runes/${rune_name}.png`
+		)
 	}
 }
 
 const otherEnts: Entity[] = []
 function DrawOther(): void {
 	for (const ent of otherEnts) {
-		if (!ent.IsVisible || (ent instanceof Unit && !ent.IsSpawned))
-			continue
+		if (!ent.IsVisible || (ent instanceof Unit && !ent.IsSpawned)) continue
 		const screen_pos = RendererSDK.WorldToScreen(ent.Position)
-		if (screen_pos === undefined)
-			continue
+		if (screen_pos === undefined) continue
 		let text = ent.Name
-		if (text.length === 0)
-			text = ent.ClassName
-		RendererSDK.Text(
-			text,
-			screen_pos,
-			GetEntityTeamColor(ent),
-		)
+		if (text.length === 0) text = ent.ClassName
+		RendererSDK.Text(text, screen_pos, GetEntityTeamColor(ent))
 	}
 }
 
 EventsSDK.on("EntityCreated", ent => {
 	if (
-		ent instanceof Ability
-		|| ent instanceof Rune
-		|| ent instanceof Hero
-		|| ent instanceof Creep
-		|| ent instanceof Tree
+		ent instanceof Ability ||
+		ent instanceof Rune ||
+		ent instanceof Hero ||
+		ent instanceof Creep ||
+		ent instanceof Tree
 	)
 		return
 	otherEnts.push(ent)
@@ -187,16 +183,12 @@ const TreesState = EntitiesNode.AddToggle("Trees", true),
 	particles = new ParticlesSDK()
 function DrawHitboxes(): void {
 	EntityManager.AllEntities.forEach(ent => {
-		if (!ent.IsVisible)
-			return
-		if (ent instanceof Tree && (!TreesState.value || !ent.IsAlive))
-			return
+		if (!ent.IsVisible) return
+		if (ent instanceof Tree && (!TreesState.value || !ent.IsAlive)) return
 		else if (ent instanceof Creep && (!CreepsState.value || !ent.IsAlive || !ent.IsSpawned))
 			return
-		else if (ent instanceof Rune && !RunesState.value)
-			return
-		else if (ent instanceof Hero && (!HeroesState.value || !ent.IsAlive))
-			return
+		else if (ent instanceof Rune && !RunesState.value) return
+		else if (ent instanceof Hero && (!HeroesState.value || !ent.IsAlive)) return
 		ent.BoundingBox.Polygon.Draw(
 			"",
 			LocalPlayer!.Hero!,
@@ -204,7 +196,7 @@ function DrawHitboxes(): void {
 			GetEntityTeamColor(ent),
 			40,
 			40,
-			false,
+			false
 		)
 	})
 }
@@ -212,20 +204,13 @@ function DrawHitboxes(): void {
 export function DrawEntities(): void {
 	const size = new Vector2(
 		GUIInfo.ScaleWidth(64, RendererSDK.WindowSize),
-		GUIInfo.ScaleHeight(64, RendererSDK.WindowSize),
+		GUIInfo.ScaleHeight(64, RendererSDK.WindowSize)
 	)
-	if (Hitboxes.value)
-		DrawHitboxes()
-	if (!Icons.value)
-		return
-	if (TreesState.value)
-		DrawTrees(size)
-	if (CreepsState.value)
-		DrawCreeps(size)
-	if (RunesState.value)
-		DrawRunes(size)
-	if (HeroesState.value)
-		DrawHeroes(size)
-	if (Other.value)
-		DrawOther()
+	if (Hitboxes.value) DrawHitboxes()
+	if (!Icons.value) return
+	if (TreesState.value) DrawTrees(size)
+	if (CreepsState.value) DrawCreeps(size)
+	if (RunesState.value) DrawRunes(size)
+	if (HeroesState.value) DrawHeroes(size)
+	if (Other.value) DrawOther()
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { DOTAGameUIState_t, EventsSDK, GameState } from "github.com/octarine-public/wrapper/index"
+import { DOTAGameUIState, EventsSDK, GameState } from "github.com/octarine-public/wrapper/index"
+
 import { DrawAttachments } from "./Attachments"
 import { DrawCursor } from "./Cursor"
 import { DrawEntities } from "./Entities"
@@ -8,8 +9,7 @@ import { DrawMinimap } from "./Minimap"
 import { DrawTopPanel } from "./TopPanel"
 
 EventsSDK.on("PreDraw", () => {
-	if (GameState.UIState !== DOTAGameUIState_t.DOTA_GAME_UI_DOTA_INGAME)
-		return
+	if (GameState.UIState !== DOTAGameUIState.DOTA_GAME_UI_DOTA_INGAME) return
 	DrawAttachments()
 	DrawEntities()
 	DrawMinimap()
